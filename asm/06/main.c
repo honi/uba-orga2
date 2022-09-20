9c03d107aced92f2462399549c233f6dc5c3a5e6
Joni Bekenstein <joni@bek.io>
2022-09-20 10:24:45 -0300

    DRY

--- asm/06/main.c
+++ asm/06/main.c
@@ -4,24 +4,21 @@
 
 typedef char* (*cesar_func_ptr)(char*, int);
 
-void call_cesar(cesar_func_ptr cesar_func, char* input, int x) {
+void call_cesar_aux(cesar_func_ptr cesar_func, char* func_name, char* input, int x) {
     char* res = (*cesar_func)(input, x);
-    printf("'%s' -> '%s'\n", input, res);
+    printf("%s(\"%s\", %d) -> \"%s\"\n", func_name, input, x, res);
     free(res);
 }
 
+void call_cesar(char* input, int x) {
+    call_cesar_aux(cesar, "    cesar", input, x);
+    call_cesar_aux(cesar_asm, "cesar_asm", input, x);
+}
+
 int main() {
-    printf("Versión C\n");
-    call_cesar(cesar, "CASA", 3);
-    call_cesar(cesar, "CALABAZA", 7);
-    call_cesar(cesar, "CALABAZA", 7 + 26 * 10);
-    call_cesar(cesar, "", 1);
-
-    printf("\nVersión ASM\n");
-    call_cesar(cesar_asm, "CASA", 3);
-    call_cesar(cesar_asm, "CALABAZA", 7);
-    call_cesar(cesar_asm, "CALABAZA", 7 + 26 * 10);
-    call_cesar(cesar_asm, "", 1);
-
+    call_cesar("CASA", 3);
+    call_cesar("CALABAZA", 7);
+    call_cesar("CALABAZA", 7 + 26 * 10);
+    call_cesar("", 1);
     return 0;
 }
